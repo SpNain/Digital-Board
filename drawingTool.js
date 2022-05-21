1d51dc1882ff0eb9c2a18fd4192704d17083117c
HardeepNain <hnspnain@gmail.com>
2022-05-21 15:31:36 +0530

    created digital board repo

---
+++ drawingTool.js
@@ -0,0 +1,88 @@
+let pen = document.querySelector("#pen");
+let eraser = document.querySelector("#eraser");
+
+let penOptions = pen.querySelector(".pen-tool-options");
+let eraserOptions = eraser.querySelector(".eraser-tool-options");
+
+let penSize = penOptions.querySelector("#pensize");
+let eraserSize = eraserOptions.querySelector("#erasersize");
+
+let penColors = penOptions.querySelectorAll(".pen-colors div");
+
+let currentPenSize = 1;
+let currentPenColor = 'black';
+let currentEraserSize = 1;
+let currentEraserColor = 'white';
+
+penSize.addEventListener("change", function() {
+    // handle pen size
+    let penSizeValue = penSize.value;
+    // console.log(penSizeValue);
+    // pensize set hoga
+    currentPenSize = penSizeValue;
+    ctx.lineWidth = currentPenSize;
+});
+
+eraserSize.addEventListener("click", function() {
+    let eraserSizeValue = eraserSize.value;
+    currentEraserSize = eraserSizeValue;
+    ctx.lineWidth = currentEraserSize;
+});
+
+for (let i = 0; i < penColors.length; i++) {
+    penColors[i].addEventListener("click", function(e) {
+        let penColor = e.target.className;
+        currentPenColor = penColor;
+        ctx.strokeStyle = currentPenColor; // for lines
+    });
+}
+
+pen.addEventListener("click", function() {
+    if (pen.classList.contains("active-tool")) {
+        // pen already active hai
+        // pen tool options open honge
+        // if (penOptions.classList.contains("hide")) {
+        //   penOptions.classList.remove("hide"); // remove hide class from penOptions
+        // } else {
+        //   penOptions.classList.add("hide");
+        // }
+
+        penOptions.classList.toggle("hide");
+    } else {
+        // pen is not active
+        // make pen active
+        eraser.classList.remove("active-tool");
+        eraser.classList.add("fade");
+        eraserOptions.classList.add("hide");
+
+        pen.classList.remove("fade");
+        pen.classList.add("active-tool");
+
+        ctx.lineWidth = currentPenSize;
+        ctx.strokeStyle = currentPenColor;
+    }
+});
+
+eraser.addEventListener("click", function() {
+    if (eraser.classList.contains("active-tool")) {
+        // eraser already active
+        // if (eraserOptions.classList.contains("hide")) {
+        //   eraserOptions.classList.remove("hide"); // remove hide class from penOptions
+        // } else {
+        //   eraserOptions.classList.add("hide");
+        // }
+
+        eraserOptions.classList.toggle("hide");
+    } else {
+        // eraser not active
+        pen.classList.remove("active-tool");
+        pen.classList.add("fade");
+        penOptions.classList.add("hide");
+
+        eraser.classList.add("active-tool");
+        eraser.classList.remove("fade");
+
+        ctx.strokeStyle = currentEraserColor;
+        ctx.lineWidth = currentEraserSize;
+    }
+});
---
+++ sticky.js
@@ -0,0 +1,110 @@
+let sticky = document.querySelector("#sticky");
+let stickyCount = 0;
+let taskbarDiv;
+sticky.addEventListener("click", function(e){  // isko humne aise "sticky.addEventListener("click", addSticky()" aise call nhi lagayi kyunki isse fir addSticky() ke apna khud ka ek event pass hota aur hum imageElement pass nhi kar paate 
+  // console.log(stickyCount);
+  addSticky();
+});
+
+function addSticky(imageElement) {
+  stickyCount++;
+  if (stickyCount == 1) {
+    taskbarDiv = document.createElement('div');
+    taskbarDiv.classList.add('s-taskbar');
+    if (document.querySelector('#canvas').classList.contains('canvas-dark')) {
+      taskbarDiv.classList.add('dark-s-taskbar');
+    } else {
+      taskbarDiv.classList.add('light-s-taskbar');
+    }
+
+    document.body.append(taskbarDiv);
+  }
+
+  let stickyDiv = document.createElement("div");
+  stickyDiv.classList.add("sticky");
+  stickyDiv.innerHTML = `<div class="sticky-header">
+  <div class="minimize"><i class="fa-solid fa-minus"></i></div>
+  <div class="close"><i class="fa-solid fa-xmark"></i></div>
+</div>`;
+
+  let minimize = stickyDiv.querySelector(".minimize");
+  let close = stickyDiv.querySelector(".close");
+  let stickyHeader = stickyDiv.querySelector(".sticky-header");
+
+  let stickyContent;   // ye alag s global bnaya kyunki ispe humne if else ke blocks ke bahar bhi kaam kar rkha hai
+  if(imageElement){
+    let stickyImageDiv = document.createElement("div");
+    stickyImageDiv.classList.add("sticky-image-div");
+    stickyDiv.append(stickyImageDiv);
+    stickyImageDiv.append(imageElement);
+    stickyContent = stickyImageDiv;
+  }else{
+     // <div class="sticky-content" contenteditable="true"></div>
+     let stickyContentDiv = document.createElement("div");
+     stickyContentDiv.classList.add("sticky-content");
+     stickyContentDiv.setAttribute("contenteditable" , "true");
+     stickyDiv.append(stickyContentDiv);
+     stickyContent = stickyContentDiv;
+  }
+
+  minimize.addEventListener("click", function () {
+    // stickyContent.style.display == "none" ? (stickyContent.style.display = "block") : (stickyContent.style.display = "none");
+    stickyDiv.style.display = "none";
+    
+    let msDiv = document.createElement('div');
+    msDiv.classList.add('msdiv');
+    msDiv.innerHTML += `<img class="s-img" src="./s-img.png" alt="">`;
+
+    msDiv.addEventListener('click', function () {
+      stickyDiv.style.display = "block";
+      msDiv.remove();
+    });
+
+    taskbarDiv.append(msDiv);
+  });
+
+  close.addEventListener("click", function () {
+    stickyCount--;
+    // console.log(stickyCount);
+    if (stickyCount == 0) {
+      taskbarDiv.remove();
+    }
+    stickyDiv.remove();
+  });
+
+  // script of sticky header motion
+  let stickyHold = false;
+  let initialX;
+  let initialY;
+  stickyHeader.addEventListener("mousedown", function (e) {
+      stickyHold=true;
+      initialX = e.clientX;
+      initialY = e.clientY;
+  });
+
+  stickyHeader.addEventListener("mousemove", function (e) {
+      if(stickyHold){
+          let finalX = e.clientX;
+          let finalY = e.clientY;
+    
+          let dx = finalX - initialX;
+          let dy = finalY - initialY;
+    
+          let {top , left} = stickyDiv.getBoundingClientRect();  // ye hume element ki diff values laake deta hai jaise top left x y etc.
+          // console.log(stickyDiv.getBoundingClientRect()); 
+          //   sticky => top + dy
+          //  sticky => left + dx
+          stickyDiv.style.top = top + dy + "px";  // elem.style se hum set kar skte hai get nhi
+          stickyDiv.style.left = left +dx + "px";
+    
+          initialX = finalX;
+          initialY = finalY;
+      }
+  });
+
+  stickyHeader.addEventListener("mouseup", function (e) {
+      stickyHold = false;
+  });
+
+  document.body.append(stickyDiv);
+}
